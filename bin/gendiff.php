00993eebb00299c35fd0dafc2876a283ea89e2c9
NikolaiProgramist <nikluk2005@mail.ru>
2024-10-14 19:42:59 +0300

    Update binary file

--- bin/gendiff.php
+++ bin/gendiff.php
@@ -19,15 +19,12 @@ Generate diff
 Usage:
   gendiff (-h|--help)
   gendiff (-v|--version)
+  gendiff [--format <fmt>] <firstFile> <secondFile>
 
 Options:
   -h --help                     Show this screen
   -v --version                  Show version
+  --format <fmt>                Report format [default: stylish]
 DOC;
 
 $args = Docopt::handle($doc, array('version' => 'Gendiff 1.0'));
-
-switch ($args) {
-    case '-h' || '--help':
-        line($doc);
-}
