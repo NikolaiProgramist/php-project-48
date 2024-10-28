5d252ec7bd15b13f6575524a473ca009b30cbc8d
NikolaiProgramist <nikluk2005@mail.ru>
2024-10-28 12:54:52 +0300

    Global refactoring and change logic of work Differ.php

--- src/Differ/Differ.php
+++ src/Differ/Differ.php
@@ -6,90 +6,101 @@ use function Functional\sort;
 use function Differ\Differ\Translator\getJson;
 use function Differ\Formatters\Stylish\stylish;
 
-const ADD_MARKER = '+';
-const REMOVE_MARKER = '-';
-const UNCHANGED_MARKER = ' ';
-
 function genDiff(string $pathToFile1, string $pathToFile2, string $format = 'stylish'): string
 {
     $firstFile = getJson($pathToFile1);
     $secondFile = getJson($pathToFile2);
 
-    $sortingFirstFile = function ($acc, $key, $tree1, $tree2, $currentSorting) {
-        if (!array_key_exists($key, $tree2)) {
-            $acc[REMOVE_MARKER . $key] = $tree1[$key];
-            return $acc;
-        }
-
-        $value1 = $tree1[$key];
-        $value2 = $tree2[$key];
-
-        if (!is_array($value1)) {
-            if (is_array($value2)) {
-                $acc[REMOVE_MARKER . $key] = $value1;
-                $acc[ADD_MARKER . $key] = $value2;
-                return $acc;
-            }
-
-            if ($value1 === $value2) {
-                $acc[UNCHANGED_MARKER . $key] = $value1;
-            } else {
-                $acc[REMOVE_MARKER . $key] = $value1;
-                $acc[ADD_MARKER . $key] = $value2;
-            }
-
-            return $acc;
-        }
-
-        if (!is_array($value2)) {
-            $acc[REMOVE_MARKER . $key] = $value1;
-            $acc[ADD_MARKER . $key] = $value2;
-            return $acc;
-        }
-
-        $innerContent = getCustomDiff($value1, $value2, $currentSorting);
-        $acc[$key] = $innerContent;
-        return $acc;
-    };
-
-    $sortingSecondFile = function ($acc, $key, $tree2, $tree1, $currentSorting) {
-        if (array_key_exists($key, $tree1)) {
-            if (is_array($tree1[$key]) && is_array($tree2[$key])) {
-                $innerContent = getCustomDiff($tree2[$key], $tree1[$key], $currentSorting);
-                $acc[$key] = $innerContent;
-                return $acc;
-            }
-
-            return $acc;
-        }
-
-        if (is_array($tree2[$key])) {
-            $acc[ADD_MARKER . $key] = $tree2[$key];
-            return $acc;
-        }
-
-        $acc[ADD_MARKER . $key] = $tree2[$key];
-        return $acc;
-    };
-
-    $elements = getCustomDiff($firstFile, $secondFile, $sortingFirstFile);
-    $elements2 = getCustomDiff($secondFile, $firstFile, $sortingSecondFile);
+    $elements = sortingFirstFile($firstFile, $secondFile);
+    $elements2 = sortingSecondFile($secondFile, $firstFile);
 
     $resultDiff = sortArrayByKeysRecursive(array_merge_recursive($elements, $elements2));
 
-    switch ($format) {
-        case 'stylish':
-            return stylish($resultDiff);
+    if ($format === 'stylish') {
+        return stylish($resultDiff);
     }
+
+    return stylish($resultDiff);
 }
 
-function getCustomDiff(array $tree1, array $tree2, callable $sorting): array
+function sortingFirstFile($tree1, $tree2)
 {
+    if (!is_array($tree1)) {
+        return $tree1;
+    }
+
+    if (!is_array($tree2)) {
+        return $tree2;
+    }
+
     return array_reduce(
         array_keys($tree1),
-        fn($acc, $key) => $sorting($acc, $key, $tree1, $tree2, $sorting),
+        function ($acc, $key) use ($tree1, $tree2) {
+            $acc[$key]['key'] = $key;
+
+            if (!array_key_exists($key, $tree2)) {
+                $acc[$key]['status'] = 'remove';
+                $innerContent = sortingFirstFile($tree1[$key], $tree1[$key]);
+                $acc[$key]['value'] = $innerContent;
+                return $acc;
+            }
+
+            if ($tree1[$key] === $tree2[$key]) {
+                $acc[$key]['status'] = 'unchanged';
+                $acc[$key]['value'] = sortingFirstFile($tree1[$key], $tree1[$key]);
+                return $acc;
+            }
+
+            if (is_array($tree1[$key]) && is_array($tree2[$key])) {
+                $acc[$key]['status'] = 'changed';
+                $acc[$key]['value'] = sortingFirstFile($tree1[$key], $tree2[$key]);
+            } else {
+                $acc[$key]['status'] = 'remove';
+                $acc[$key]['beforeValue'] = sortingFirstFile($tree1[$key], $tree1[$key]);
+                $acc[$key]['afterValue'] = sortingFirstFile($tree2[$key], $tree2[$key]);
+            }
+
+            return $acc;
+        },
         []
     );
+}
+
+function sortingSecondFile($tree2, $tree1)
+{
+    if (!is_array($tree2)) {
+        return $tree2;
+    }
+
+    return array_reduce(
+        array_keys($tree2),
+        function ($acc, $key) use ($tree1, $tree2) {
+            if (!array_key_exists($key, $tree1)) {
+                $acc[$key]['value'] = getArrayContent($tree2[$key]);
+                return $acc;
+            }
+
+            if (is_array($tree2[$key]) && is_array($tree1[$key])) {
+                $acc[$key]['value'] = sortingSecondFile($tree2[$key], $tree1[$key]);
+                return $acc;
+            }
+
+            return $acc;
+        },
+        []
+    );
+}
+
+function getArrayContent($tree)
+{
+    if (!is_array($tree)) {
+        return $tree;
+    }
+
+    return array_reduce(array_keys($tree), function ($acc, $key) use ($tree) {
+        $acc[$key]['value'] = getArrayContent($tree[$key]);
+        return $acc;
+    }, []);
 }
 
 function sortArrayByKeysRecursive(array $tree): array
@@ -97,20 +108,7 @@ function sortArrayByKeysRecursive(array $tree): array
     $keysSorted = sort(
         array_keys($tree),
         function ($left, $right) {
-            $l = $left;
-            $r = $right;
-
-            $markers = [ADD_MARKER, REMOVE_MARKER, UNCHANGED_MARKER];
-
-            if (in_array($left[0], $markers)) {
-                $l = substr($left, 1);
-            }
-
-            if (in_array($right[0], $markers)) {
-                $r = substr($right, 1);
-            }
-
-            return strcmp($l, $r);
+            return strcmp($left, $right);
         },
         true
     );
--- src/Formatters/Stylish.php
+++ src/Formatters/Stylish.php
@@ -2,33 +2,60 @@
 
 namespace Differ\Formatters\Stylish;
 
+const ADD_MARKER = '+';
+const REMOVE_MARKER = '-';
+const UNCHANGED_MARKER = ' ';
+
 function stylish($tree, string $replacer = ' ', int $spacesCount = 4, int $depth = 1): string
 {
     $child = array_reduce(
         array_keys($tree),
         function ($acc, $key) use ($tree, $replacer, $spacesCount, $depth) {
-            $keyName = $key;
+            $keyData = $tree[$key];
+            $status = $tree[$key]['status'] ?? 'add';
+
+            $status = match ($status) {
+                'add' => ADD_MARKER,
+                'remove' => REMOVE_MARKER,
+                default => UNCHANGED_MARKER
+            };
+
             $indentationCount = $spacesCount * $depth - 2;
             $indentation = str_repeat($replacer, $indentationCount);
 
-            $markers = ['+', '-', ' '];
-            $marker = ' ';
+            if (array_key_exists('value', $keyData)) {
+                $value = $keyData['value'];
 
-            if (in_array($key[0], $markers)) {
-                $marker = $key[0];
-                $keyName = substr($key, 1);
-            }
+                if (!is_array($value)) {
+                    $acc .= getStylishString($status, $key, $value, $indentation);
+                    return $acc;
+                }
 
-            if (!is_array($tree[$key])) {
-                $value = toString($tree[$key]);
-                $value = $value === '' ? '' : " {$value}";
-                $acc .= $indentation . "{$marker} {$keyName}:{$value}\n";
+                if ($status === ADD_MARKER) {
+                    $innerContent = getArrayContent($value, $replacer, $spacesCount, $depth + 1);
+                    $acc .= getStylishInnerContent($status, $key, $innerContent, $indentation);
+                    return $acc;
+                }
+
+                $innerContent = stylish($keyData['value'], $replacer, $spacesCount, $depth + 1);
+                $acc .= getStylishInnerContent($status, $key, $innerContent, $indentation);
                 return $acc;
             }
 
-            $innerContent = stylish($tree[$key], $replacer, $spacesCount, $depth + 1);
-            $acc .= $indentation . "{$marker} {$keyName}: {\n{$innerContent}";
-            $acc .= $indentation . "  }\n";
+            if (!is_array($keyData['beforeValue'])) {
+                $acc .= getStylishString(REMOVE_MARKER, $key, $keyData['beforeValue'], $indentation);
+            } else {
+                $innerContent = stylish($keyData['beforeValue'], $replacer, $spacesCount, $depth + 1);
+                $acc .= getStylishInnerContent($status, $key, $innerContent, $indentation);
+            }
+
+            if (!is_array($keyData['afterValue'])) {
+                $acc .= getStylishString(ADD_MARKER, $key, $keyData['afterValue'], $indentation);
+            } else {
+                $innerContent = stylish($keyData['afterValue'], $replacer, $spacesCount, $depth + 1);
+                $acc .= getStylishInnerContent($status, $key, $innerContent, $indentation);
+            }
+
             return $acc;
         },
         ''
@@ -41,7 +68,42 @@ function stylish($tree, string $replacer = ' ', int $spacesCount = 4, int $depth
     return "{$child}";
 }
 
-function toString($string): string
+function getArrayContent($tree, string $replacer, int $spacesCount, int $depth): string
+{
+    return array_reduce(array_keys($tree), function ($acc, $key) use ($tree, $replacer, $spacesCount, $depth) {
+        $value = $tree[$key]['value'];
+        $indentationCount = $spacesCount * $depth - 2;
+        $indentation = str_repeat($replacer, $indentationCount);
+
+        if (!is_array($value)) {
+            $acc .= getStylishString(UNCHANGED_MARKER, $key, $value, $indentation);
+            return $acc;
+        }
+
+        $innerContent = getArrayContent($value, $replacer, $spacesCount, $depth + 1);
+        return getStylishInnerContent(UNCHANGED_MARKER, $key, $innerContent, $indentation);
+    }, '');
+}
+
+function getStylishInnerContent(string $marker, string $key, string $innerContent, string $indentation): string
+{
+    $result = $indentation . $marker . " {$key}: {\n{$innerContent}";
+    $result .= $indentation . "  }\n";
+    return $result;
+}
+
+function getStylishString(string $marker, string $key, $value, string $indentation): string
+{
+    $keyValue = getString($value);
+
+    if ($value === '') {
+        return $indentation . "{$marker} {$key}:\n";
+    } else {
+        return $indentation . "{$marker} {$key}: {$keyValue}\n";
+    }
+}
+
+function getString($string): string
 {
     if (is_bool($string)) {
         return $string ? 'true' : 'false';
