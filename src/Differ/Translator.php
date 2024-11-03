bed158a4dc5433d70a57550c39179dce5b36f88a
NikolaiProgramist <nikluk2005@mail.ru>
2024-11-03 14:25:41 +0300

    Fix

--- src/Differ/Translator.php
+++ src/Differ/Translator.php
@@ -4,8 +4,6 @@ namespace Differ\Translator;
 
 use Symfony\Component\Yaml\Yaml;
 
-use function Differ\Parser\parseError;
-
 function getJson(string $path): array
 {
     $content = getFileContent($path);
@@ -21,24 +19,10 @@ function getFileContent(string $path): string
 {
     $dirPath = __DIR__;
 
-    // Colors for beautiful :)
-    $normalColor = "\x1b[0m";
-    $redColor = "\x1b[41m";
-
     if (str_starts_with($path, '/')) {
         $fullPath = $path;
     } else {
         $fullPath = "{$dirPath}/../../{$path}";
-    }
-
-    if (!file_exists($fullPath)) {
-        $errorMessage = <<<DOC
-        {$redColor}ERROR!{$normalColor}
-        
-        File: "{$fullPath}" don't exists!
-        DOC;
-
-        parseError($errorMessage);
     }
 
     return file_get_contents($fullPath);
--- src/Parser.php
+++ src/Parser.php
@@ -2,24 +2,11 @@
 
 namespace Differ\Parser;
 
-use JetBrains\PhpStorm\NoReturn;
-
 use function cli\line;
 use function Differ\Differ\genDiff;
 
-#[NoReturn] function parse(string $string): void
-{
-    line('%s', $string);
-    exit();
-}
-
-#[NoReturn] function parseDiff(string $pathToFile1, string $pathToFile2, string $format): void
+function parse(string $pathToFile1, string $pathToFile2, string $format): void
 {
     $resultString = genDiff($pathToFile1, $pathToFile2, $format);
-    parse($resultString);
-}
-
-#[NoReturn] function parseError(string $errorMessage): void
-{
-    parse($errorMessage);
+    line($resultString);
 }
