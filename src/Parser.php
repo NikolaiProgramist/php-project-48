571887567a71272a3f391f2dfc72bb8e1eff989a
NikolaiProgramist <nikluk2005@mail.ru>
2024-11-03 14:33:27 +0300

    Fix

--- src/Parser.php
+++ src/Parser.php
@@ -2,11 +2,10 @@
 
 namespace Differ\Parser;
 
-use function cli\line;
 use function Differ\Differ\genDiff;
 
 function parse(string $pathToFile1, string $pathToFile2, string $format): void
 {
     $resultString = genDiff($pathToFile1, $pathToFile2, $format);
-    line($resultString);
+    echo $resultString;
 }
