8cf41143d32f35ccb583d5b0d448b293407fcef8
NikolaiProgramist <nikluk2005@mail.ru>
2024-11-03 14:14:07 +0300

    Add #[NoReturn] for functions

--- src/Parser.php
+++ src/Parser.php
@@ -2,22 +2,23 @@
 
 namespace Differ\Parser;
 
+use JetBrains\PhpStorm\NoReturn;
 use function cli\line;
 use function Differ\Differ\genDiff;
 
-function parse(string $string): void
+#[NoReturn] function parse(string $string): void
 {
     line('%s', $string);
     exit();
 }
 
-function parseDiff(string $pathToFile1, string $pathToFile2, string $format): void
+#[NoReturn] function parseDiff(string $pathToFile1, string $pathToFile2, string $format): void
 {
     $resultString = genDiff($pathToFile1, $pathToFile2, $format);
     parse($resultString);
 }
 
-function parseError(string $errorMessage): void
+#[NoReturn] function parseError(string $errorMessage): void
 {
     parse($errorMessage);
 }
